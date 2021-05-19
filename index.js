9bb84fe8e5c1b304b8636b67837d5d5981a468c6
Lai-Raksa <80080049+Lai-Raksa@users.noreply.github.com>
2021-05-19 15:19:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+ // Add active class to the current button (highlight it)
+       var header = document.getElementById("navbar-nav");
+       var example = header.getElementsByClassName("nav-act");
+       for (var i = 0; i < example.length; i++) {
+        example[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+        });
+       }
